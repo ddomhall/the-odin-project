3cbcd3ac6a251d3966c5cdafc48a5f8942cc8fdf
ddomhall <domjshall@gmail.com>
2024-01-06 09:49:37 +0000

    not module and all tests written

--- test-practice.test.js
+++ test-practice.test.js
@@ -3,3 +3,22 @@ import { capitalize, reverseString, calculator, caesarCipher, analyseArray } fro
 test('capitalize', () => {
   expect(capitalize('test')).toBe('Test')
 })
+
+test('reverseString', () => {
+  expect(reverseString('test')).toBe('tset')
+})
+
+test('calculator', () => {
+  expect(calculator.add(3, 3)).toBe(6)
+  expect(calculator.subtract(3, 3)).toBe(0)
+  expect(calculator.divide(3, 3)).toBe(1)
+  expect(calculator.multiply(3, 3)).toBe(9)
+})
+
+test('caesarCipher', () => {
+  expect(caesarCipher('z.a', 1)).toBe('a.b')
+})
+
+test('analyseArray', () => {
+  expect(analysArray([0, 1, 2])).toBe({average: 1, min: 0, max: 2, length: 3})
+})
