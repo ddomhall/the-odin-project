7c7eabf4a0ce84a549c4e34f1fb42c7d5f1c621f
ddomhall <domjshall@gmail.com>
2024-01-11 20:05:34 +0000

    handlesubmit

--- react/cv/src/App.jsx
+++ react/cv/src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 
 function App() {
+  const [data, setData] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const obj = {}
+    const formData = new FormData(e.currentTarget)
+    for (let [key, value] of formData.entries()) {
+      obj[key] = value
+    }
+    setData([...data, obj])
+    e.currentTarget.reset()
+  }
 
   return (
     <>
-      <form id="general" className="flex flex-col gap-6">
-        <input placeholder="name"/>
-        <input placeholder="email" type="email"/>
-        <input placeholder="number" type="tel"/>
+      <form id="general" onSubmit={handleSubmit}>
+        <input name="name" placeholder="name" required/>
+        <input name="email" placeholder="email" type="email" required/>
+        <input name="number" placeholder="number" type="tel" required/>
         <input type="submit" value="Submit"/>
       </form>
     </>
