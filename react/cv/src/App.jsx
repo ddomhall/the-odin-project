14fb002e51c7a3e989bd95dbd8d728742a0838f4
ddomhall <domjshall@gmail.com>
2024-01-12 18:34:35 +0000

    edit functional

--- react/cv/src/App.jsx
+++ react/cv/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 
 function App() {
-  const [data, setData] = useState({name: '', email: '', number: ''})
+  const [data, setData] = useState({name: '', email: '', number: '', edit: true})
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    const obj = {}
+    const obj = {edit: false}
     const formData = new FormData(e.currentTarget)
     for (let [key, value] of formData.entries()) {
       obj[key] = value
@@ -14,18 +14,28 @@ function App() {
     e.currentTarget.reset()
   }
 
-  return (
-    <>
-      {data.name != '' ? <section><div>name: {data.name}</div><div>email: {data.email}</div><div>number: {data.number}</div><button>Edit</button></section> : 
-      <form id="general" onSubmit={handleSubmit}>
-        <input name="name" placeholder="name" required/>
-        <input name="email" placeholder="email" type="email" required/>
-        <input name="number" placeholder="number" type="tel" required/>
-        <button>Submit</button>
-      </form>
-      }
-    </>
-  )
+  const edit = () => {
+    setData({...data, edit: true})
+  }
+
+  if (data.edit) {
+    return (
+      <>
+        <form id="general" onSubmit={handleSubmit}>
+          <input name="name" placeholder="name" defaultValue={data.name} required/>
+          <input name="email" placeholder="email" type="email" defaultValue={data.email} required/>
+          <input name="number" placeholder="number" type="tel" defaultValue={data.number} required/>
+          <button>Submit</button>
+        </form>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <section><div>name: {data.name}</div><div>email: {data.email}</div><div>number: {data.number}</div><button onClick={edit}>Edit</button></section>
+      </>
+    )
+  }
 }
 
 export default App
