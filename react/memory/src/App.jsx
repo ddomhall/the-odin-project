2ac181ba12c1c6a35aa8f8203080dcb73330bbb4
ddomhall <domjshall@gmail.com>
2024-01-13 17:42:27 +0000

    score updates

--- react/memory/src/App.jsx
+++ react/memory/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import Card from './components/Card.jsx'
 
 export default function App() {
+  const [scores, setScores] = useState({score: 0, best: 0})
   const titles = [1, 2, 3]
+
+  const handleClick = (e) => {
+    setScores({...scores, score: scores.score + 1})
+  }
+
   return (
     <>
-      Hello world
-      {titles.map(t => <Card title={t}/>)}
+      <p>score: {scores.score}</p>
+      <p>best: {scores.best}</p>
+      <main className="flex gap-6">
+      {titles.map(t => <Card key={t} title={t} f={handleClick}/>)}
+      </main>
     </>
   )
 }
--- react/memory/src/components/Card.jsx
+++ react/memory/src/components/Card.jsx
@@ -1,3 +1,3 @@
-export default function Card({title}) {
-  return <div>Card {title}</div>
+export default function Card({title, f}) {
+  return <div onClick={f} className="ring ring-white rounded-xl w-auto aspect-square p-2">Card {title}</div>
 }
