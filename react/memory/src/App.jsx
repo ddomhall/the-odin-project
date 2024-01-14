69d528b1c853f1f703ecdc9c12ed5854f97162f8
ddomhall <domjshall@gmail.com>
2024-01-14 09:47:17 +0000

    memory done

--- react/memory/src/App.jsx
+++ react/memory/src/App.jsx
@@ -1,23 +1,28 @@
 import {useState, useEffect} from 'react'
 import Card from './components/Card.jsx'
-import sig from '/sig.png'
-import btpfp from '/btpfp.jpg'
-import track from '/track.png'
 
 export default function App() {
-  const [images, setImages] = useState([{img: sig, key: 0}, {img: btpfp, key: 1}, {img: track, key: 2}])
+  const [images, setImages] = useState([])
   const [scores, setScores] = useState({score: 0, best: 0})
   const [clicked, setClicked] = useState([])
 
-  // useEffect(() => {
-  //   const data = fetch('https://api.giphy.com/v1/gifs/random?api_key=qdBh39Uw5FuONO8Xa5MuCBVH5btTBH6z&tag=&rating=g', {mode: 'cors'})
-  //   .then(function(response) {
-  //     return response.json();
-  //   })
-  //   .then(function(response) {
-  //     setImages([...images, response.data.images.fixed_height.url])
-  //   });
-  // }, [])
+  async function getImages() {
+    const promises = []
+    const newImages = []
+    for (let i = 0; i < 10; i++) {
+      promises.push(fetch('https://api.giphy.com/v1/gifs/random?api_key=39BVPnodKgEtMeqYkH4A65OWGayOHJGg&tag=&rating=g', {mode: 'cors'}))
+    }
+
+
+    const res = await Promise.all(promises)
+    const data = await Promise.all(res.map(r => r.json()))
+      .then(res => res.map(i => newImages.push({img: i.data.images.fixed_height.url, key: Math.random()})))
+      .then(res => setImages(newImages))
+  }
+
+  useEffect(() => {
+    getImages()
+  }, [])
 
   useEffect(() => {
     setImages(images.sort((a, b) => 0.5 - Math.random()))
@@ -35,7 +40,7 @@ export default function App() {
 
   return (
     <>
-      <p>{ scores.score == 3 ? 'you win' : 'score: ' + scores.score + ' | best: ' + scores.best}</p>
+      <p>{ scores.score == 10 ? 'you win' : 'score: ' + scores.score + ' | best: ' + scores.best}</p>
       <main className="flex gap-6 flex-wrap">
         {images.map(i => <Card key={i.key} i={i.img} f={handleClick}/>)}
       </main>
