6723666409139cf981ca127e1e337d1c58e249c6
ddomhall <domjshall@gmail.com>
2024-01-14 08:48:41 +0000

    score working

--- react/memory/src/App.jsx
+++ react/memory/src/App.jsx
@@ -1,22 +1,33 @@
 import {useState, useEffect} from 'react'
 import Card from './components/Card.jsx'
+import sig from '../public/sig.png'
+import btpfp from '../public/btpfp.jpg'
+import track from '../public/track.png'
 
 export default function App() {
-  const [images, setImages] = useState([])
+  const [images, setImages] = useState([{img: sig, id: 0}, {img: btpfp, id: 1}, {img: track, id: 2}])
   const [scores, setScores] = useState({score: 0, best: 0})
+  const [clicked, setClicked] = useState([])
 
-  useEffect(() => {
-    const data = fetch('https://api.giphy.com/v1/gifs/random?api_key=qdBh39Uw5FuONO8Xa5MuCBVH5btTBH6z&tag=&rating=g', {mode: 'cors'})
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-      setImages([...images, response.data.images.fixed_height.url])
-    });
-  }, [])
-
+  // useEffect(() => {
+  //   const data = fetch('https://api.giphy.com/v1/gifs/random?api_key=qdBh39Uw5FuONO8Xa5MuCBVH5btTBH6z&tag=&rating=g', {mode: 'cors'})
+  //   .then(function(response) {
+  //     return response.json();
+  //   })
+  //   .then(function(response) {
+  //     setImages([...images, response.data.images.fixed_height.url])
+  //   });
+  // }, [])
+  //
   const handleClick = (e) => {
-    setScores({...scores, score: scores.score + 1})
+    console.log(e)
+    if (clicked.includes(e.target.src)) {
+      setScores({score: 0, best: scores.score > scores.best ? scores.score : scores.best})
+      setClicked([])
+    } else {
+      setScores({...scores, score: scores.score + 1})
+      setClicked([...clicked, e.target.src])
+    }
   }
 
   return (
@@ -24,7 +35,7 @@ export default function App() {
       <p>score: {scores.score}</p>
       <p>best: {scores.best}</p>
       <main className="flex gap-6 flex-wrap">
-        {images.map(i => <Card key={i} i={i} f={handleClick}/>)}
+        {images.map(i => <Card key={i.id} i={i.img} f={handleClick}/>)}
       </main>
     </>
   )
--- react/memory/src/components/Card.jsx
+++ react/memory/src/components/Card.jsx
@@ -1,3 +1,3 @@
 export default function Card({i, f}) {
-  return <img src={i} alt="img" onClick={f} className="ring ring-white rounded-xl w-auto aspect-square p-2"/>
+  return <img src={i} alt="img" onClick={f} className="ring ring-white rounded-xl p-2"/>
 }
