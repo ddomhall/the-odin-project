5177d844d6f5f3daceddd885b6066b93e81c44c7
ddomhall <domjshall@gmail.com>
2024-01-15 08:32:28 +0000

    sidebar + tailwind

--- react/shopping/src/App.jsx
+++ react/shopping/src/App.jsx
@@ -3,13 +3,17 @@ import { Outlet, Link } from "react-router-dom";
 export default function App() {
 
   return (
-    <>
-      <div>app</div>
-      <Link to='/'>Home</Link>
-      <hr/>
-      <Link to='/shopping'>Shopping</Link>
-      <hr/>
-      <Outlet />
-    </>
+    <div className="grid grid-cols-5 h-screen">
+      <div className="col-span-4">
+        <Outlet />
+      </div>
+      <aside className="col-span-1 flex flex-col justify-between">
+        <div>
+        <h1>Cart</h1>
+        <section>items</section>
+        </div>
+        <button>Check Out</button>
+      </aside>
+    </div>
   )
 }
--- react/shopping/src/Shopping.jsx
+++ react/shopping/src/Shopping.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Shopping() {
-  return <div>Shopping</div>
+  return <Link to='/'>Home</Link>
 }
