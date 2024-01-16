2e5977ea8adf42b20b87bfff8025c756279c19d8
ddomhall <domjshall@gmail.com>
2024-01-16 08:31:35 +0000

    input quantity

--- react/shopping/src/Item.jsx
+++ react/shopping/src/Item.jsx
@@ -3,22 +3,13 @@ import {useState} from 'react'
 export default function Item({i, f}) {
   const [quantity, setQuantity] = useState(1)
 
-  function addQuantity() {
-    setQuantity(q => q + 1)
-  }
-
-  function subQuantity() {
-    if (quantity != 1) setQuantity(q => q - 1)
-  }
-
   return (
-    <div className='flex flex-col w-32 h-32' data-value={i.id}>
+    <div className='flex flex-col w-32 h-24' data-value={i.id}>
       <div>name: {i.name}</div>
       <div>price: {i.price}</div>
-      <div className='flex justify-evenly' data-value={quantity}>
-        <button className='w-full' onClick={subQuantity}>-</button>
-        <div className='w-full text-center'>{quantity}</div>
-        <button className='w-full' onClick={addQuantity}>+</button>
+      <div className='flex' data-value={quantity}>
+        <label for={'q' + i.id}>quantity: </label>
+        <input id={'q' + i.id} className='w-full text-center' type='number' value={quantity} onChange={e => setQuantity(e.target.value)}/>
       </div>
       <button onClick={f}>add to cart</button>
     </div>
