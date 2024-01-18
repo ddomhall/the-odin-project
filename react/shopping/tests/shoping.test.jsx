7dc35f7376f2a5f98e6c0497cf07a2067192057d
ddomhall <domjshall@gmail.com>
2024-01-18 20:02:57 +0000

    item callback tested

--- react/shopping/tests/shoping.test.jsx
+++ react/shopping/tests/shoping.test.jsx
@@ -1,6 +1,8 @@
+import {vi} from 'vitest'
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CartItem from './../src/CartItem.jsx'
+import Item from './../src/Item.jsx'
 
 describe('cartitem', () => {
   it('renders all data', () => {
@@ -9,5 +11,20 @@ describe('cartitem', () => {
     expect(screen.getByTestId('ci-title').textContent).toMatch('title1')
     expect(screen.getByTestId('ci-quantity').textContent).toMatch('1')
     expect(screen.getByTestId('ci-price').textContent).toMatch('1')
+  })
+})
+
+describe('item', () => {
+  it('calls callback', async () => {
+    const onClick = vi.fn();
+    const props = {id: 1, title: 'title1', description: 'desc1', image: 'image1', price: 1}
+    const user = userEvent.setup()
+
+    render(<Item i={props} f={onClick} />)
+
+    const button = screen.getByRole("button");
+    await user.click(button);
+
+    expect(onClick).toHaveBeenCalled()
   })
 })
