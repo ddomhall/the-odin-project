a2ca58dda14853db5a9cfd4eb94bcde2e33c40f0
ddomhall <domjshall@gmail.com>
2024-01-19 18:08:16 +0000

    cart price 2dp

--- react/shopping/src/CartItem.jsx
+++ react/shopping/src/CartItem.jsx
@@ -3,7 +3,7 @@ export default function CartItem({i}) {
   <div>
       <div data-testid='ci-title'>title: {i.item.title}</div>
       <div data-testid='ci-quantity'>quantity: {i.quantity}</div>
-      <div data-testid='ci-price'>price: {i.item.price * i.quantity}</div>
+      <div data-testid='ci-price'>price: {(Math.round(i.item.price * i.quantity * 100) / 100).toFixed(2)}</div>
   </div>
   )
 }
