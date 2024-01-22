2e76d8e04ea8d4ef03a2522a916471515db971f2
ddomhall <domjshall@gmail.com>
2024-01-22 05:07:00 +0000

    data to template

--- node/message/routes/index.js
+++ node/message/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'hi there',
+    user: 'amando',
+    added: new Date()
+  },
+  {
+    text: 'hello world',
+    user: 'charles',
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'message', messages: messages });
 });
 
 module.exports = router;
