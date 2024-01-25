49099bcb0a16fdb008c30a25002dfa1bcd1f7a87
ddomhall <domjshall@gmail.com>
2024-01-25 08:27:55 +0000

    books in category list

--- node/inventory/controllers/categoryController.js
+++ node/inventory/controllers/categoryController.js
@@ -1,14 +1,21 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 const Category = require("../models/category");
+const Item = require("../models/item");
 
 exports.category_list = asyncHandler(async (req, res, next) => {
   const allCategories = await Category.find().sort({name: 1}).exec()
+
   res.render('category_list', {title: 'Category List', category_list: allCategories})
 });
 
 exports.category_detail = asyncHandler(async (req, res, next) => {
-  res.send('not implemented')
+  const [category, books] = await Promise.all([
+  Category.findById(req.params.id).exec(),
+  Item.find({ category: req.params.id }, "name description").exec(),
+  ])
+
+  res.render('category_detail', {category: category, books: books})
 });
 
 exports.category_create_get = (req, res, next) => {
