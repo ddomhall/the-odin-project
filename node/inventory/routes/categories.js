6c2913fbfa17f3959947d659f372da2d18fac8e5
ddomhall <domjshall@gmail.com>
2024-01-26 05:18:03 +0000

    fixed route order bug

--- node/inventory/routes/categories.js
+++ node/inventory/routes/categories.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const category_controller = require("../controllers/categoryController");
 
 router.get("/", category_controller.category_list);
-router.get("/:id", category_controller.category_detail);
 router.get("/create", category_controller.category_create_get);
 router.post("/create", category_controller.category_create_post);
+router.get("/:id", category_controller.category_detail);
 router.get("/:id/update", category_controller.category_update_get);
 router.post("/:id/update", category_controller.category_update_post);
 router.get("/:id/delete", category_controller.category_delete_get);
--- node/inventory/routes/items.js
+++ node/inventory/routes/items.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const item_controller = require("../controllers/itemController");
 
 router.get("/", item_controller.item_list);
-router.get("/:id", item_controller.item_detail);
 router.get("/create", item_controller.item_create_get);
 router.post("/create", item_controller.item_create_post);
+router.get("/:id", item_controller.item_detail);
 router.get("/:id/update", item_controller.item_update_get);
 router.post("/:id/update", item_controller.item_update_post);
 router.get("/:id/delete", item_controller.item_delete_get);
