091bfc40229f8012ce363e3d281e357129515490
ddomhall <domjshall@gmail.com>
2024-01-26 05:56:43 +0000

    item form and create get

--- node/inventory/controllers/itemController.js
+++ node/inventory/controllers/itemController.js
@@ -1,6 +1,7 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 const Item = require("../models/item");
+const Categories = require("../models/category");
 
 exports.item_list = asyncHandler(async (req, res, next) => {
   const items = await Item.find().sort({name: 1}).exec()
@@ -12,9 +13,10 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
   res.render('item_detail', {item: item})
 });
 
-exports.item_create_get = (req, res, next) => {
-  res.send('not implemented')
-};
+exports.item_create_get = asyncHandler(async(req, res, next) => {
+  categories = await Categories.find().sort({name: 1}).exec()
+  res.render('item_form', {title: 'Create Item', categories: categories})
+});
 
 exports.item_create_post = (req, res, next) => {
   res.send('not implemented')
