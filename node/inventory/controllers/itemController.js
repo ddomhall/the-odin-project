663aa201ed2fc2f5dffe5d2c1c02b9e17677affc
ddomhall <domjshall@gmail.com>
2024-01-26 04:35:47 +0000

    item read views

--- node/inventory/controllers/itemController.js
+++ node/inventory/controllers/itemController.js
@@ -3,11 +3,13 @@ const asyncHandler = require("express-async-handler");
 const Item = require("../models/item");
 
 exports.item_list = asyncHandler(async (req, res, next) => {
-  res.send('not implemented')
+  const items = await Item.find().sort({name: 1}).exec()
+  res.render('item_list', {title: 'All Items', items: items})
 });
 
 exports.item_detail = asyncHandler(async (req, res, next) => {
-  res.send('not implemented')
+  const item = await Item.findById(req.params.id).exec()
+  res.render('item_detail', {item: item})
 });
 
 exports.item_create_get = (req, res, next) => {
