cae64347cf1ec6e70c0b3ea0c4755ea0b51e445c
ddomhall <domjshall@gmail.com>
2024-01-27 06:46:53 +0000

    signup validation

--- node/clubhouse/app.js
+++ node/clubhouse/app.js
@@ -1,10 +1,46 @@
 const express = require('express')
+const { body, validationResult } = require('express-validator');
 const app = express()
+app.use(express.urlencoded({ extended: false }));
+
 app.set("views", "./views");
 app.set('view engine', 'ejs')
+
 let session = false
 let user = {fname: '', lname: '', email: ''}
-app.get('/', (req, res, next) => res.render('index', {user}))
-app.get('/signup', (req, res, next) => res.render('signup', {session, user}))
-app.get('/login', (req, res, next) => res.render('login', {user}))
+
+app.get('/', (req, res, next) => res.render('index', {session}))
+
+app.get('/signup', (req, res, next) => res.render('signup', {session, user, errors:''}))
+
+app.post('/signup', [
+
+	body('fname', 'first name empty').trim().notEmpty().escape(),
+	body('lname', 'last name empty').trim().notEmpty().escape(),
+	body('email', 'invalid email').trim().isEmail().escape(),
+	body('pw', 'invalid password').trim().isLength({min: 3}).escape(),
+	body('cpw', 'invalid password').trim().isLength({min: 3}).escape(),
+
+	async(req, res, next) => {
+		const errors = validationResult(req).array()
+
+		user = {
+			fname: req.body.fname,
+			lname: req.body.lname,
+			email: req.body.email,
+		}
+
+		if (errors.length || req.body.pw != req.body.cpw) {
+			if (req.body.pw != req.body.cpw) {
+				errors.push({msg: 'passwords must match'})
+			}
+			res.render('signup', {session, user, errors})
+		} else {
+			res.redirect('/')
+		}
+	}
+])
+
+app.get('/login', (req, res, next) => res.render('login', {session, user}))
+
 app.listen(3000, () => console.log("listening on :3000"));
