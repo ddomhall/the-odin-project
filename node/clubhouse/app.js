0b2dd1e598cf27d14b32769b09f5dd3490c91c25
ddomhall <domjshall@gmail.com>
2024-01-27 05:14:55 +0000

    views

--- node/clubhouse/app.js
+++ node/clubhouse/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res, next) => res.send('test'))
+app.set("views", "./views");
+app.set('view engine', 'ejs')
+app.get('/', (req, res, next) => res.render('index'))
 app.listen(3000, () => console.log("listening on :3000"));
