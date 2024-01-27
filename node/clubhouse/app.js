99a907a91669fae17de07d8105226287844790f3
ddomhall <domjshall@gmail.com>
2024-01-27 10:37:58 +0000

    hashed pw login

--- node/clubhouse/app.js
+++ node/clubhouse/app.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const { body, validationResult } = require('express-validator');
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const bcrypt = require('bcryptjs')
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs")
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const mongoDb = "mongodb+srv://admin:3TC2vdxJvezI8ETb@cluster0.ty3uuu8.mongodb.net/clubhouse?retryWrites=true&w=majority"
 mongoose.connect(mongoDb);
@@ -10,70 +13,96 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
 const User = mongoose.model(
-	"User",
-	new Schema({
-		username: { type: String, required: true },
-		pw: { type: String, required: true },
-		member: { type: Boolean, required: true },
+  "User",
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+  })
+);
+
+const app = express();
+app.set("views", 'views');
+app.set("view engine", "ejs");
+
+app.use(session({ secret: 'dom', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+passport.use(
+	new LocalStrategy(async (username, password, done) => {
+		try {
+			const user = await User.findOne({ username: username });
+			if (!user) {
+				return done(null, false, { message: "Incorrect username" });
+			};
+			const match = await bcrypt.compare(password, user.password);
+			if (!match) {
+				return done(null, false, { message: "Incorrect password" })
+			}
+			return done(null, user);
+		} catch(err) {
+			return done(err);
+		};
 	})
 );
 
-const app = express()
-app.use(express.urlencoded({ extended: false }));
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
 
-app.set("views", "./views");
-app.set('view engine', 'ejs')
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  };
+});
 
-let session = false
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
-app.get('/', (req, res, next) => res.render('index', {session}))
+app.get("/", (req, res) => {
+  res.render("index", { session: req.user });
+});
 
-app.get('/signup', (req, res, next) => res.render('signup', {user: {username: ''}, errors:''}))
+app.get("/signup", (req, res) => res.render("signup", {user: {username: ''}, errors: ''}));
 
-app.post('/signup', [
-
-	body('username', 'username empty').trim().notEmpty().escape().custom(async value => {
-		const user = await User.findOne({username: value}).exec()
-		if (user) {
-			throw new Error('username in use')
-		}
-	}),
-	body('pw', 'invalid password(s)').trim().notEmpty().escape().custom((value, {req}) => {
-		return value === req.body.cpw
-	}),
-
-	(req, res, next) => {
-		const errors = validationResult(req).array()
-
-		bcrypt.hash(req.body.pw, 8, async function(err, hash) {
-
+app.post("/signup", async (req, res, next) => {
+	bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+		if (err) {
+			return next(err);
+		} else {
 			const user = new User({
 				username: req.body.username,
-				pw: hash,
-				member: false
-			})
+				password: hashedPassword
+			});
+			const result = await user.save();
+			res.redirect("/");
+		};
+	});
+});
 
-			if (errors.length) {
-			res.render('signup', {user, errors})
-			} else {
-				await user.save()
-				res.redirect('/')
-			}
-		})
-	}
-])
+app.get("/login", (req, res) => res.render("login", {user: {username: ''}, errors: ''}));
 
-app.get('/login', (req, res, next) => res.render('login', {user: {username: ''}, errors: ''}))
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+  })
+);
 
-app.post('/login', async (req, res, next) => {
-	const user = await User.findOne({username: req.body.username}).exec()
-	if (!user) {
-		res.render('login', {user: {username: req.body.username}, errors: [{msg: 'username not found'}]})
-	} else if (req.body.pw !== user.pw) {
-		res.render('login', {user: {username: req.body.username}, errors: [{msg: 'incorrect password'}]})
-	} else {
-		res.redirect('/')
-	}
-})
+app.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
-app.listen(3000, () => console.log("listening on :3000"));
+app.listen(3000, () => console.log("app listening on port 3000!"));
