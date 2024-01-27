bd586c79006fd886aad85ac5d69b08b8a14fd9b3
ddomhall <domjshall@gmail.com>
2024-01-27 05:50:44 +0000

    signup form

--- node/clubhouse/app.js
+++ node/clubhouse/app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 app.set("views", "./views");
 app.set('view engine', 'ejs')
-let user = false
+let session = false
+let user = {fname: '', lname: '', email: ''}
 app.get('/', (req, res, next) => res.render('index', {user}))
-app.get('/signup', (req, res, next) => res.render('signup', {user}))
+app.get('/signup', (req, res, next) => res.render('signup', {session, user}))
 app.get('/login', (req, res, next) => res.render('login', {user}))
 app.listen(3000, () => console.log("listening on :3000"));
