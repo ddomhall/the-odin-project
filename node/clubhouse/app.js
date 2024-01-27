6c3aabfd083e3ac88f9f4376ac355ba465263378
ddomhall <domjshall@gmail.com>
2024-01-27 05:33:07 +0000

    nav bar

--- node/clubhouse/app.js
+++ node/clubhouse/app.js
@@ -2,5 +2,8 @@ const express = require('express')
 const app = express()
 app.set("views", "./views");
 app.set('view engine', 'ejs')
-app.get('/', (req, res, next) => res.render('index'))
+let user = false
+app.get('/', (req, res, next) => res.render('index', {user}))
+app.get('/signup', (req, res, next) => res.render('signup', {user}))
+app.get('/login', (req, res, next) => res.render('login', {user}))
 app.listen(3000, () => console.log("listening on :3000"));
