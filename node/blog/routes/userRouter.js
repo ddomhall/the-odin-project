e2ff97b3cf9ea35671f317e6e7ec690bbd3f9f50
ddomhall <domjshall@gmail.com>
2024-01-28 07:15:26 +0000

    hash pws

--- node/blog/routes/userRouter.js
+++ node/blog/routes/userRouter.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/userModel.js')
+const bcrypt = require('bcryptjs')
 
 router.get('/', async (req, res) => {
 	res.json(await User.find({}, 'username following').exec())
 })
 
 router.post('/', async (req, res) => {
-	const user = await new User({
-		username: req.body.username,
-		password: req.body.password,
-		following: [],
-	}).save()
-	res.json({msg: 'user created'})
+	bcrypt.hash(req.body.password, 8, async function(err, hash) {
+		if (err) res.status(500)
+		const user = await new User({
+			username: req.body.username,
+			password: hash,
+			following: [],
+			}).save()
+		res.json({msg: 'user created'})
+	})
 })
 
 module.exports = router
