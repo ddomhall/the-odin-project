fcc0d8090989041cd70bae40223e7d74cd285d0d
ddomhall <domjshall@gmail.com>
2024-01-29 07:03:26 +0000

    logout

--- node/blog/client/src/App.jsx
+++ node/blog/client/src/App.jsx
@@ -1,11 +1,15 @@
-import {Outlet, useLocation} from 'react-router-dom'
+import {Outlet} from 'react-router-dom'
 import {useState, useContext} from 'react'
 import {SessionContext} from './SessionContext.jsx'
 import Cookies from 'js-cookie'
 
 function App() {
-  const location = useLocation()
   const [session, setSession] = useState(Cookies.get('session'))
+
+  function logout() {
+    Cookies.remove('session')
+    window.location.reload()
+  }
 
   return (
     <>
@@ -16,7 +20,7 @@ function App() {
             <input name='search' />
             <input type='submit' value='search' />
           </form>
-          {session ? <a href={'/users/' + session }>profile</a> : location.pathname == '/login' ? <a href='/signup'>sign up</a> : <a href='/login'>log in</a>}
+          {session ? <div><a href={'/users/' + session }>profile</a><button onClick={logout}>log out</button></div> : <div><a href='/signup'>sign up</a><a href='/login'>log in</a></div>}
         </nav>
         <main className='w-96 m-auto'>
           <Outlet />
