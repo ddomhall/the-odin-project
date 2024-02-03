32db4561f78c2686cd447707a36a976af59e763b
ddomhall <domjshall@gmail.com>
2024-02-03 11:56:13 +0000

    select menu and cancel

--- node/photo/src/routes/Play.jsx
+++ node/photo/src/routes/Play.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 
 export default function Play() {
   const [characters, setCharacters] = useState([
@@ -49,28 +49,43 @@ export default function Play() {
     },
   ])
   const [guess, setGuess] = useState({})
-  const x = 120
+  const guessRef = useRef(null)
 
   function logXY(e) {
     // setGuess({x: Math.round((e.pageX/e.target.width)*100), y: Math.round(((e.pageY-80)/e.target.height)*100)})
+    guessRef.current.style.display = 'block'
     setGuess({x: e.pageX, y: e.pageY-80})
+  }
+
+  function cancelGuess() {
+    guessRef.current.style.display = 'none'
   }
 
   return (
     <>
       <section className='grid grid-cols-2 grid-rows-1'>
-        <div className='flex leading-10'>
+        <div className='flex leading-10 justify-between'>
           Not found:
           {characters.filter(c => !c.found).map(c => <img src={c.name + '.png'} key={c.id} className='h-10 w-10'/>)}
         </div>
-        <div className='flex'>
+        <div className='flex leading-10 justify-between'>
           Found:
           {characters.filter(c => c.found).map(c => <img src={c.name + '.png'} key={c.id} className='h-10 w-10'/>)}
         </div>
       </section>
       <section className='relative'>
-      <img src='opwaldo.png' onClick={logXY}/>
-      {!!guess ? <div className='absolute h-12 w-12 border-4 border-black' style={{left: guess.x-24, top: guess.y-24}}></div> : ''}
+        <img src='opwaldo.png' onClick={logXY}/>
+        {!!guess ?
+          <div className='hidden' ref={guessRef}>
+            <div className='absolute h-12 w-12 border-4 border-black pointer-events-none' style={{left: guess.x-24, top: guess.y-24}}></div>
+            <div className='fixed top-0 bg-black w-full'>
+              <p className='text-center h-10 leading-10'>who did you find?</p>
+              <div className='flex'>
+                {characters.filter(c => !c.found).map(c => <button className='flex w-full leading-10'><img src={c.name + '.png'} className='h-10 w-10'/>{c.name}</button>)}
+                <button className='h-10 w-full' onClick={cancelGuess}>cancel</button>
+              </div>
+            </div>
+          </div> : ''}
       </section>
     </>
   )
