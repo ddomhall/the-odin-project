a098b1000b105fd1f8694fdc7088f9c92290430a
ddomhall <domjshall@gmail.com>
2024-02-03 18:37:05 +0000

    can't guess offscreen

--- node/photo/api/routes/index.js
+++ node/photo/api/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({m: 1})
+router.get('/guess', function(req, res, next) {
+  const name = req.query.name
+  const x = req.query.x
+  const y = req.query.y
+
+  res.json({name, x, y})
 });
 
 module.exports = router;
--- node/photo/client/src/routes/Play.jsx
+++ node/photo/client/src/routes/Play.jsx
@@ -50,55 +50,79 @@ export default function Play() {
   ])
   const [guess, setGuess] = useState({x: 0, y: 0})
   const guessRef = useRef(null)
+  const headerRef = useRef(null)
+  const imageRef = useRef(null)
 
   useEffect(() => {
-    fetch('http://localhost:3000/').then(res => res.json()).then(res => console.log(res))
+    // fetch('http://localhost:3000/').then(res => res.json()).then(res => console.log(res))
   }, [])
 
   function placeGuess(e) {
     // setGuess({x: Math.round((e.pageX/e.target.width)*100), y: Math.round(((e.pageY-80)/e.target.height)*100)})
     guessRef.current.style.display = 'block'
-    setGuess({x: e.pageX, y: e.pageY-80})
+    let x
+    let y
+    if (e.pageX < 24) {
+      x = 24
+    } else if (e.pageX > imageRef.current.clientWidth - 24) {
+      x = imageRef.current.clientWidth - 24
+    } else {
+      x = e.pageX
+    }
+
+    if (e.pageY < 24 + 40 + headerRef.current.clientHeight) {
+      y = 24 + 40 + headerRef.current.clientHeight
+    } else if (e.pageY > 40 + headerRef.current.clientHeight + imageRef.current.clientHeight - 24) {
+      y = 40 + headerRef.current.clientHeight + imageRef.current.clientHeight - 24
+    } else {
+      y = e.pageY
+    }
+
+    setGuess({x, y: y-40-headerRef.current.clientHeight})
+    console.log(guess)
   }
 
   function cancelGuess() {
     guessRef.current.style.display = 'none'
   }
 
-  function submitGuess(e) {
-    console.log(e.target.textContent)
+  async function submitGuess(e) {
+    // console.log(e.target.textContent)
+    const x = Math.round(guess.x / (imageRef.current.clientWidth)*100)
+    const y = Math.round(guess.y / (imageRef.current.clientHeight)*100)
+    console.log(x, y)
     cancelGuess()
   }
 
   return (
     <>
-      <section className='grid grid-cols-2 grid-rows-1'>
-        <div className='flex leading-10 justify-between'>
+      <section className='grid grid-cols-2 grid-rows-1' ref={headerRef}>
+        <div className='flex leading-10 justify-between flex-wrap'>
           Not found:
           {characters.filter(c => !c.found).map(c => <img src={c.name + '.png'} key={c.id} className='h-10 w-10'/>)}
         </div>
-        <div className='flex leading-10 justify-between'>
+        <div className='flex leading-10 justify-between flex-wrap'>
           Found:
           {characters.filter(c => c.found).map(c => <img src={c.name + '.png'} key={c.id} className='h-10 w-10'/>)}
         </div>
       </section>
       <section className='relative'>
-        <img src='opwaldo.png' onClick={placeGuess}/>
+        <img src='opwaldo.png' onClick={placeGuess} ref={imageRef}/>
         {!!guess ?
           <div className='hidden' ref={guessRef}>
             <div className='absolute h-12 w-12 border-4 border-black pointer-events-none' style={{left: guess.x-24, top: guess.y-24}}></div>
-            <div className='fixed top-0 bg-black w-full'>
+            <div className='fixed top-0 bg-black w-screen'>
               <p className='text-center h-10 leading-10'>who did you find?</p>
-              <div className='flex'>
+              <div className='flex flex-wrap justify-between'>
                 {characters.filter(c => !c.found).map(c => {
                   return (
-                    <div key={c.id} onClick={submitGuess} className='flex w-full leading-10'>
-                      <img src={c.name + '.png'} className='h-10 w-10  pointer-events-none'/>
-                      <p className='pointer-events-none w-full'>{c.name}</p>
+                    <div key={c.id} onClick={submitGuess} className='flex leading-10'>
+                      <img src={c.name + '.png'} className='h-10 w-10 pointer-events-none'/>
+                      <p className='pointer-events-none w-20'>{c.name}</p>
                     </div>
                   )
                 })}
-                <button className='h-10 w-full' onClick={cancelGuess}>cancel</button>
+                <button className='h-10 w-20' onClick={cancelGuess}>cancel</button>
               </div>
             </div>
           </div> : ''}
