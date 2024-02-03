5a548e46e1756e1f4f26741b67a9043bc3eb91b8
ddomhall <domjshall@gmail.com>
2024-02-03 07:38:23 +0000

    image and coords

--- node/photo/src/routes/Play.jsx
+++ node/photo/src/routes/Play.jsx
@@ -1,3 +1,9 @@
 export default function Play() {
-  return <div>Play</div>
+  function logXY(e) {
+    console.log(Math.round((e.pageX/e.target.width)*100), Math.round(((e.pageY-48)/e.target.height)*100))
+  }
+
+  return (
+    <img src='opwaldo.png' onClick={logXY}/>
+  )
 }
