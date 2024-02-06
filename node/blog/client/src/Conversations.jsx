4fefe21bef4071767c4b18067a50429b9c8970c3
ddomhall <domjshall@gmail.com>
2024-02-06 06:14:34 +0000

    show conversation list

--- node/blog/client/src/Conversations.jsx
+++ node/blog/client/src/Conversations.jsx
@@ -4,18 +4,24 @@ import {Link, useParams} from 'react-router-dom'
 
 export default function Message() {
   const [conversations, setConversations] = useState([])
+  const [user, setUser] = useState({})
   const {session} = useContext(SessionContext)
 
   useEffect(() => {
-    fetch('http://localhost:3000/conversations', {
+    async function getUserConversations() {
+    await fetch('http://localhost:3000/conversations', {
       credentials: 'include',
-    }).then(res => res.json()).then(res => setConversations(res))
+    }).then(res => res.json()).then(res => {
+        setConversations(res.map(c => c = {id: c._id, users: c.users.map(u => u.username).join(', ')}))
+      })
+    }
+    getUserConversations()
   }, [])
 
   return (
     <div className='grid grid-cols-3'>
-      <aside>
-        {conversations ? conversations.map(c => <Link to={'/conversations/' + c._id} key={c._id}>{c.users.map(u => u.username).join(', ')}</Link>) : ''}
+      <aside className='flex flex-col'>
+        {conversations ? conversations.map(c => <Link to={'/conversations/' + c.id} key={c.id}>{c.users}</Link>) : ''}
       </aside>
       <section className='col-span-2'>
         section
